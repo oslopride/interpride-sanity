c5f433e37afc7d1a4cf83ae3dbb7ac78b2464b8f
karl.georgsen <karl.georgsen@makingwaves.com>
2019-10-29 20:26:53 +0100

    Added slug to page schema

--- schemas/pages.js
+++ schemas/pages.js
@@ -9,6 +9,14 @@ export default {
       type: "string"
     },
     {
+      title: "Slug",
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+      }
+    },
+    {
       title: "Layout",
       name: "layout",
       type: "array",
